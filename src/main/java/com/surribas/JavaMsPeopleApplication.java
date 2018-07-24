1b9f59830c97db56c0d041da7698a5dd4e3b4db1
Unknown <ricardo.surribas@gmail.com>
2018-07-24 17:11:23 -0300

    Upload 0.0.3 Version
    - Eureka Discover

--- src/main/java/com/surribas/JavaMsPeopleApplication.java
+++ src/main/java/com/surribas/JavaMsPeopleApplication.java
@@ -2,12 +2,14 @@ package com.surribas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import com.surribas.backbone.config.MicroservicesCommunicationConfig;
 import com.surribas.backbone.model.Person;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import({MicroservicesCommunicationConfig.class})
 public class JavaMsPeopleApplication {
 	
